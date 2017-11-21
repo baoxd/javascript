d25609ff44d8dcac9298052ec09ac506ed528bde
baoxd <1459815090@qq.com>
2017-11-21 20:27:22 +0800

    update es5

--- es6/arraybuffer/1.js
+++ es6/arraybuffer/1.js
@@ -16,3 +16,18 @@ function getPlatformEndianness() {
       throw new Error('Unknown endianness');
   }
 }
+
+// ArrayBuffer转为字符串，参数为ArrayBuffer对象
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
+
+// 字符串转为ArrayBuffer对象，参数为字符串
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length * 2); // 每个字符占用2个字节
+  var bufView = new Uint16Array(buf);
+  for (var i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
