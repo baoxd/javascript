b94c264521dcd5cca677a655b092a7c0ebdd3dba
baoxd <1459815090@qq.com>
2018-07-16 21:05:47 +0800

    add rematch

---
+++ rematch-demo/webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: ['./index.js'],
+    output: {
+        path: path.join(__dirname, './'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.less', '.scss', '.css'],
+        modules: ['node_modules']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js[x]?$/,
+                use: [{
+                    loader: "babel-loader"
+                }]
+            }
+        ],
+    },
+    devServer: {
+        port: 8080,
+        contentBase: ".",
+        hot: true,
+        historyApiFallback: true,
+        publicPath: "/",
+        stats: {
+            colors: true
+        }
+    }
+}
+
